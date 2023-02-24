e96a80e5b0820f934e2d644e66b78300b4f1ede9
Daniil Kisel <danilakisel@gmail.com>
2023-02-24 19:32:13 +0300

    upd test

--- src/lean/Main.lean
+++ src/lean/Main.lean
@@ -2,23 +2,22 @@ import Raylib
 
 open Raylib
 
-partial def loop : IO Unit := do
-  BeginDrawing
-  do
-    ClearBackground RAYWHITE
-    DrawFPS 0 0
-    let mp ← GetMousePosition
-    DrawRectangleGradientEx (Rectangle.mk 100.0 100.0 mp.x mp.y) RED GREEN BLUE YELLOW
-  EndDrawing
-
-  let exit ← WindowShouldClose
-  if exit
-    then pure ()
-    else loop
-
 def main : IO Unit := do
   SetConfigFlags FLAG_VSYNC_HINT
   InitWindow 640 480 "Hello, Raylib-Lean"
+  InitAudioDevice
   SetExitKey KEY_NULL
-  loop
+
+  repeat do
+    BeginDrawing
+    do
+      ClearBackground RAYWHITE
+      DrawFPS 0 0
+      let mp ← GetMousePosition
+      DrawRectangleGradientEx (Rectangle.mk 100.0 100.0 mp.x mp.y) RED GREEN BLUE YELLOW
+    EndDrawing
+
+    if (← WindowShouldClose) then break
+
+  CloseAudioDevice
   CloseWindow
