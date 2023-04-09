1045c71b088c6e326fdba8fb352f0330f0b84337
Daniil Kisel <danilakisel@gmail.com>
2023-04-09 13:47:07 +0300

    Put newlines between alias definitions

--- src/lean/Raylib/Aliases.lean
+++ src/lean/Raylib/Aliases.lean
@@ -6,11 +6,15 @@ namespace Raylib
 
 /-- Texture2D, same as Texture -/
 abbrev Texture2D : Type := Texture
+
 abbrev Texture2DRef : Type := TextureRef
+
 /-- TextureCubemap, same as Texture -/
 abbrev TextureCubemap : Type := Texture
+
 /-- RenderTexture2D, same as RenderTexture -/
 abbrev RenderTexture2D : Type := RenderTexture
+
 /-- Camera type fallback, defaults to Camera3D -/
 abbrev Camera : Type := Camera3D
 
